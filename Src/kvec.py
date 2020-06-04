c26fb92aaf4d9e852c188ab66ac6f062734bd087
kazushi <kimoto@okayama-u.ac.jp>
2020-06-04 17:48:21 +0900

    modiflying kvec.py

--- Src/kvec.py
+++ Src/kvec.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
+from mpl_toolkits.axes_grid1.colorbar import colorbar
 
 class Img:
     def load(self,fname):
@@ -7,6 +9,7 @@ class Img:
 
         fp.readline()
         self.freq=float(fp.readline())
+        print("f=",self.freq,"[MHz]")
 
         fp.readline()
         dat=fp.readline()
@@ -31,10 +34,13 @@ class Img:
         dx[1]=float(dat[1]);
         fp.readline()
 
-        A=[];
+        A=[];B=[];
         for row in fp:
-            A.append(float(row))
+            dat=row.strip().split(",")
+            A.append(float(dat[0]))
+            B.append(float(dat[1]))
         A=np.array(A)
+        B=np.array(B)
         fp.close()
 
         self.Ndiv=Ndiv
@@ -42,48 +48,53 @@ class Img:
         self.dx=dx
         self.Wd=self.dx*self.Ndiv
         self.Xb=self.Xa+self.Wd
-        self.A=np.reshape(A,[Nx,Ny])
+        self.A=np.transpose(np.reshape(A,[Nx,Ny]))
+        self.B=np.transpose(np.reshape(B,[Nx,Ny]))
+        self.Nx=Nx;
+        self.Ny=Ny;
     def show(self,ax):
         ax.imshow(self.A,cmap="jet",origin="lower",aspect="equal");
 
 if __name__=="__main__":
 
     Kx=Img()    # create Img class instace
-    Ky=Img()    # create Img class instance
 
-    num=5     # file No.
-    fname1="kx"+str(num)+".out" # kx field data file
-    fname2="ky"+str(num)+".out" # ky field data file
-
-    Kx.load(fname1) # load kx data
-    Ky.load(fname2) # load ky data
-    #print(Kx.Wd)
+    num=201     # file No.
+    fname="k"+str(num)+".out" # ky field data file
+    Kx.load(fname) # load k field data
 
 
     ## -------------- Scatter Plot ---------------
-    fig=plt.figure()
-    ax=fig.add_subplot(111)
+    fig1=plt.figure()
+    ax=fig1.add_subplot(211)
+    ax2=fig1.add_subplot(212)
     ndat=Kx.Ndiv[0]*Kx.Ndiv[1]; # Number of data
     xi0=np.reshape(-Kx.A,[ndat,1])  # straighten kx data
-    xi1=np.reshape(-Ky.A,[ndat,1])  # straighten ky data
+    xi1=np.reshape(-Kx.B,[ndat,1])  # straighten ky data
     ax.plot(xi0,xi1,".",markersize=2)   #  show scatter plot of (kx,ky)
     ax.grid(True)
     ax.set_aspect(1.0)
     ax.set_xlabel("kx[/mm]");
     ax.set_ylabel("ky[/mm]");
 
-    ## -------------- Scatter Plot ---------------
+
     fig2=plt.figure()
     bx=fig2.add_subplot(211)
     cx=fig2.add_subplot(212)
     xi=np.sqrt(xi0*xi0+xi1*xi1) # wave number (magnitude)
     alph=np.angle(xi0+1j*xi1)/np.pi*180.    # wave number direction
 
+    ax2.plot(alph,xi,".",markersize=2)
+    ax2.grid(True)
+    #ax2.set_aspect(1.0)
+    ax2.set_xlabel("angle")
+    ax2.set_ylabel("wave number")
+
     nbin=50
     hist_xi,bins=np.histogram(xi,bins=nbin,range=(0.01,1.5),density=False) # weights=wgt
     bin_xi=0.5*(bins[0:-1]+bins[1:])
     #bx.hist(xi,bins=50)
-    bx.plot(bin_xi,hist_xi,label=fname1)
+    bx.plot(bin_xi,hist_xi,label=fname)
     bx.grid(True);
 
     hist_alph,bins=np.histogram(alph,bins=nbin,density=False) #weight=wgt
@@ -104,12 +115,15 @@ if __name__=="__main__":
     print("x=",x)
     print("y=",y)
     #[X,Y]=np.meshgrid(x,y)
-    C=np.sqrt(Ky.A*Ky.A+Kx.A*Kx.A)
-    K=np.abs(Ky.A+1j*Kx.A);
-    A=np.angle(-(Ky.A+1j*Kx.A));
+    C=np.sqrt(Kx.A*Kx.A+Kx.B*Kx.B)
+    K=np.abs(Kx.A+1j*Kx.B);
+    A=np.angle(-(Kx.A+1j*Kx.B));
     Fx=np.cos(A)/K;
     Fy=np.sin(A)/K;
     ext=[y[0],y[-1],x[0],x[-1]]
+
+    ex.imshow(A,extent=ext,cmap="jet",interpolation="bilinear",origin="lower")
+    plt.show()
     ex.quiver(y,x,-Ky.A,-Kx.A,C,cmap="jet")
     #ex.imshow(C, extent=ext, cmap="gray",origin="lower")
     ex.imshow(K,extent=ext,cmap="gray",interpolation="bilinear",origin="lower",vmin=0,vmax=6)
